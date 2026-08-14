ae44285283e8b71b80de2e6fd7feb487cec086c2
Naila <nazuairo@gmail.com>
2026-08-14 15:59:10 +0700

    AIHHH

--- app.py
+++ app.py
@@ -40,9 +40,9 @@ except Exception as e:
     print(f"[WARNING] db.create_all() gagal: {e}")
 
 # ini jgn lupa di isi
-# supabase_url = os.environ.get('SUPABASE_URL')
-# supabase_key = os.environ.get('SUPABASE_KEY')
-# supabase = create_client(supabase_url, supabase_key)
+supabase_url = os.environ.get('SUPABASE_URL')
+supabase_key = os.environ.get('SUPABASE_KEY')
+supabase = create_client(supabase_url, supabase_key)
 
 WIB = ZoneInfo('Asia/Jakarta')
 
