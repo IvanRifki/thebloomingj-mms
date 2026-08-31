841086c56a5bb9c85de9dd1ff52211c49ab77cc0
Naila <nazuairo@gmail.com>
2026-08-31 18:53:12 +0700

    untrack refresh token script, update gitignore

--- get_refresh_token.py
+++ get_refresh_token.py
@@ -5,7 +5,7 @@ SCOPES = ["https://www.googleapis.com/auth/gmail.send"]
 flow = InstalledAppFlow.from_client_config(
     {
         "installed": {
-            "client_id": "INI_CLIENT_ID",
+            "client_id": "INI_ID_CLIENT",
             "client_secret": "INI_CLIENT_SECRET",
             "auth_uri": "https://accounts.google.com/o/oauth2/auth",
             "token_uri": "https://oauth2.googleapis.com/token",
