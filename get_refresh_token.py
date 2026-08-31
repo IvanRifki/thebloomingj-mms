7685d2cb53218330284f1d28b39ddfebcbaf3376
Naila <nazuairo@gmail.com>
2026-08-31 18:27:45 +0700

    hmm

--- get_refresh_token.py
+++ get_refresh_token.py
@@ -1,14 +1,22 @@
 from google_auth_oauthlib.flow import InstalledAppFlow
 
-SCOPES = ['https://www.googleapis.com/auth/gmail.send']
+SCOPES = ["https://www.googleapis.com/auth/gmail.send"]
 
-flow = InstalledAppFlow.from_client_secrets_file(
-    'client_secret.json',
-    SCOPES
+flow = InstalledAppFlow.from_client_config(
+    {
+        "installed": {
+            "client_id": "INI_CLIENT_ID",
+            "client_secret": "INI_CLIENT_SECRET",
+            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+            "token_uri": "https://oauth2.googleapis.com/token",
+            "redirect_uris": ["http://localhost"]
+        }
+    },
+    scopes=SCOPES
 )
 
 creds = flow.run_local_server(port=0)
 
-print("REFRESH TOKEN:", creds.refresh_token)
-print("CLIENT ID:", creds.client_id)
-print("CLIENT SECRET:", creds.client_secret)
+print("\n=== REFRESH TOKEN BARU ===")
+print(creds.refresh_token)
+print("===========================\n")
